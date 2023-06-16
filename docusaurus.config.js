4b49fe9d74f2cfb72e49ce0ce3c9d1afbd1943cc
hosomikai <showking111@gmail.com>
2023-06-16 17:40:35 +0800

    ci(config): add trailingSlash: false

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,6 +20,7 @@ const config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'HosomiKai', // Usually your GitHub org/user name.
   projectName: 'hosomikai.github.io', // Usually your repo name.
+  trailingSlash: false,
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
