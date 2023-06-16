334c799444c895864be0d9146c9f33c81e27577b
hosomikai <showking111@gmail.com>
2023-06-16 17:48:57 +0800

    ci(config): add deployment branch

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -21,6 +21,7 @@ const config = {
   organizationName: 'HosomiKai', // Usually your GitHub org/user name.
   projectName: 'hosomikai.github.io', // Usually your repo name.
   trailingSlash: false,
+  deploymentBranch: 'gh-pages',
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
